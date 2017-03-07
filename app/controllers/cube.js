9d4e45635a1bbd41b0ab7c9977f4ec8b78ce854f
Israel Martinez <israel.martinez.vargas@gmail.com>
2017-03-07 11:53:16 -0800

    pivot updated

--- app/controllers/cube.js
+++ app/controllers/cube.js
@@ -2,20 +2,49 @@ var express = require('express');
 var router 	= express.Router();
 var config	= require('../../config');
 var db = require('../db');
+var ObjectID = require('mongodb').ObjectID;
+var utils = require('../utils');
 
-router.get('/data', function (req, res, next) {
+router.get('/index', function (req, res, next) {
 
-	var dataCollection 	= db.get().collection(config.dataCollection);
+	let collection 	= db.get().collection(config.dataCollection);
 
-	dataCollection.aggregate([
-			{ $project: { "_id": 0  } }
-			], { allowDiskUse: true, cursor: { batchSize: 100000 } }).toArray(function (err, data) {
-			if (err) {
-				next(err)
-			}
+	collection.aggregate([
+		{ $project: { "_id": 0  } }
+		], { allowDiskUse: true, cursor: { batchSize: 100000 } }).toArray(function (err, data) {
+		if (err) next(err);
+
 		res.json(data);
 	});
 
+});
+
+router.get('/generate', function (req, res, next) {
+
+	let collection 	= db.get().collection(config.dataCollection);
+	let count = 10000;
+	let items = [
+	  { "product" : "Samsung Galaxy S", "os" : "1.0.2", "price" : "500" },
+	  { "product" : "Samsung Galaxy Nexus", "os" : "5.0.1", "price" : "400" },
+	  { "product" : "Samsung Galaxy S III", "os" : "4.4.2", "price" : "300" },
+	  { "product" : "Samsung Galaxy S5", "os" : "4.4.2", "price" : "200" },
+	];
+	let itemsLenght = items.length - 1;
+
+	for (let i = 0; i < 10000; i++) {
+	  let random = utils.getRandom(1, itemsLenght);
+	  let row = items[random];
+		let objectId = new ObjectID();
+
+		row._id = objectId;
+  	row.date = new Date();
+
+  	collection.insert(row, function(err, result) {
+			if (err) next(err);
+			count--;
+			if (count === 0) res.json({ success: true })
+		});
+	};
 });
 
 module.exports = router;
--- public/js/app.js
+++ public/js/app.js
@@ -2,12 +2,34 @@
 	var app = angular.module('App', ['ngAnimate']);
 	app.controller('MainCtrl', function ($scope, $http, $q) {
 
+		$scope.loader =  true;
+
+		$scope.generate = function () {
+			$scope.loader =  true;
+			generate()
+				.then(function (response) {
+					setup();
+				}).catch(function (response) {
+					alert("Request error")
+				});
+		}
+
+		function generate () {
+			var defer = $q.defer();
+			$http.get('/api/generate').then(function (response) {
+					defer.resolve(response);
+				}, function (response) {
+					defer.reject('Failed to get data. ' + response);
+				});
+			return defer.promise;
+		}
+
 		/**
 		 * HTTP request to API
 		 */
 		function request () {
 			var defer = $q.defer();
-			$http.get('/api/data').then(function (response) {
+			$http.get('/api/index').then(function (response) {
 					defer.resolve(response);
 				}, function (response) {
 					defer.reject('Failed to get data. ' + response);
@@ -15,13 +37,18 @@
 			return defer.promise;
 		};
 
-		request()
-			.then(function (response) {
-				var data = response.data;
-				pivotUI(data);
-			}).catch(function (response) {
-				alert("Request error")
-			});
+		/**
+		 *  Setup request
+		 */
+		function setup () {
+			request()
+				.then(function (response) {
+					var data = response.data;
+					pivotUI(data);
+				}).catch(function (response) {
+					alert("Request error")
+				});
+		}
 
 		/**
 		 * Pivot Init
@@ -29,11 +56,14 @@
 		function pivotUI (data) {
 			$("#output").pivotUI(
 		    data, {
-	        rows: ["DatabaseSource"],
-	        cols: ["Product"],
-		    }, false, "en"
+	        rows: ["product"],
+	        cols: ["os"],
+		    }, true, "en"
 			);
+			$scope.loader = false;
 		}
+
+		setup();
 
 	});
 })();
